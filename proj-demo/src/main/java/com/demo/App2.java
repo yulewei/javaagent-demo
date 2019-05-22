da0df4ea86fd3b94f41be3cf725b41e86b9a88cd
yulewei <yulewei@gmail.com>
2019-05-23 00:17:31 +0800

    update App2

--- proj-demo/src/main/java/com/demo/App2.java
+++ proj-demo/src/main/java/com/demo/App2.java
@@ -2,7 +2,7 @@ package com.demo;
 
 public class App2 {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         while (true) {
             System.out.println(getGreeting());
         }
